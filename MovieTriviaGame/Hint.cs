081d64ed05763ddb2c02f96f0db569d30543730b
Caleb <caleb.ohara@student.sl.on.ca>
2026-01-25 16:48:37 -0500

    Initial commit

---
+++ MovieTriviaGame/Hint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalProject
+{
+    //Hint class that contains a hint and the associated celebrity
+    internal class Hint
+    {
+        public string HintText {  get; set; }
+        public Celebrity Celebrity { get; set; }
+    }
+}
---
+++ MovieTriviaGame/Quote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalProject
+{
+    //Quote class that contains a quote, celebrity, and film
+    internal class Quote
+    {
+        public string MovieQuote {  get; set; }
+        public Celebrity Celebrity { get; set; }
+        public Film Film { get; set; }
+        public string url { get; set; }
+
+    }
+
+}
